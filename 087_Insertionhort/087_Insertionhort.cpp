9e582a5b6be1a3916a4abbfba6280ae30116e888
ASUS <ASUS@AZIZDWIMUKTI>
2024-03-15 15:05:17 +0700

    InsertionSort

--- 087_Insertionhort/087_Insertionhort.cpp
+++ 087_Insertionhort/087_Insertionhort.cpp
@@ -1,20 +1,70 @@
-// 087_Insertionhort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
+int arr[12];
+int n;
+void input() {  //procedur untuk input
+
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array : "; //output ke layar
+		cin >> n; //input dari pengguna
+		if (n <= 20) // jika n kurang dari atau sama dengan 20
+			break; // keluar dari Loop
+		else { // jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // output ke layar
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) { // Looping dengan i dimulai dari 0 hingga n-1
+		cout << "data ke-" << (i + 1) << ": "; //Output ke layar
+		cin >> arr[i]; // output dari pengguna
+
+	}
+}
+void insertionSort() {
+
+    int i, n, temp, j;
+    int arr[12];
+
+	for (i = 1; i <= n - 1; i++); { //step 1
+        temp = arr[i]; //step 2
+        j = i - 1; // step 3
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j + 1] = arr[j]; // step 4a
+            j = j - 1; // step 4b
+        }
+        arr[j + 1] = temp; // step 5
+
+
+    }
+}
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j]; // menampilkan array
+		if (j < n - 1) {
+			cout << "-->";
+		}
+	}
+	cout << endl;
+	cout << endl;
+
+	cout << " jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+	cout << endl;
+	cout << endl;
+
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    system("pause");
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
